545740cad0d5f318ccf3325a3828d415089645e0
Manzi.alex <alexbagabe@gmail.com>
2025-07-23 20:06:39 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+const tabLinks = document.getElementsByClassName('tab-link');
+const tabContents = document.getElementsByClassName('tab-content');
+
+function openTab(tabNmae){
+
+    for( tablink of tabLinks){
+        link.classList.remove('active');
+
+    }
+
+}
